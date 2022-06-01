35dc88e7e64b87e87c862db4a25d873ad8fd3581
leandrocgsi <leandrocgsi@gmail.com>
2022-06-01 20:36:22 -0300

    Implementing pagination search

--- 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controller/withyaml/PersonControllerYamlTest.java
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controller/withyaml/PersonControllerYamlTest.java
@@ -410,6 +410,46 @@ public class PersonControllerYamlTest extends AbstractIntegrationTest {
 				.statusCode(403);
 	}
 	
+
+	@Test
+	@Order(9)
+	public void testHATEOAS() throws JsonMappingException, JsonProcessingException {
+		
+		var content = given().spec(specification)
+				.config(
+						RestAssuredConfig
+							.config()
+							.encoderConfig(EncoderConfig.encoderConfig()
+								.encodeContentTypeAs(
+									TestConfigs.CONTENT_TYPE_YML,
+									ContentType.TEXT)))
+				.contentType(TestConfigs.CONTENT_TYPE_YML)
+				.accept(TestConfigs.CONTENT_TYPE_YML)
+				.queryParams("page", 3, "size", 10, "direction", "asc")
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+						.asString();
+		System.out.println(content);
+		assertTrue(content.contains ("rel: \"self\"\r\n"
+				+ "    href: \"http://localhost:8888/api/person/v1/677\""));
+		assertTrue(content.contains("rel: \"self\"\r\n"
+				+ "    href: \"http://localhost:8888/api/person/v1/846\""));
+		assertTrue(content.contains("rel: \"self\"\r\n"
+				+ "    href: \"http://localhost:8888/api/person/v1/714\""));
+		
+		assertTrue(content.contains("rel: \"first\"\r\n  href: \"http://localhost:8888/api/person/v1?direction=asc&page=0&size=10&sort=firstName,asc\""));
+		assertTrue(content.contains("rel: \"prev\"\r\n  href: \"http://localhost:8888/api/person/v1?direction=asc&page=2&size=10&sort=firstName,asc\""));
+		assertTrue(content.contains("rel: \"self\"\r\n  href: \"http://localhost:8888/api/person/v1?page=3&size=10&direction=asc\""));
+		assertTrue(content.contains("rel: \"next\"\r\n  href: \"http://localhost:8888/api/person/v1?direction=asc&page=4&size=10&sort=firstName,asc\""));
+		assertTrue(content.contains("rel: \"last\"\r\n  href: \"http://localhost:8888/api/person/v1?direction=asc&page=100&size=10&sort=firstName,asc\""));
+		
+		assertTrue(content.contains("page:\r\n  size: 10\r\n  totalElements: 1007\r\n  totalPages: 101\r\n  number: 3"));
+	}
+	
 	private void mockPerson() {
 		person.setFirstName("Nelson");
 		person.setLastName("Piquet");
