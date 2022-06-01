63baeaf245c4fce7a73fd75868878fbc55e19630
leandrocgsi <leandrocgsi@gmail.com>
2022-06-01 09:05:11 -0300

    Implementing Upload and Download Files

--- 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exceptions.FileStorageException;
+import br.com.erudio.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -47,6 +50,17 @@ public class FileStorageService {
 		} catch (Exception e) {
 			throw new FileStorageException(
 				"Could not store file " + filename + ". Please try again!", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String filename) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) return resource;
+			else throw new MyFileNotFoundException("File not found");
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found" + filename, e);
 		}
 	}
 
