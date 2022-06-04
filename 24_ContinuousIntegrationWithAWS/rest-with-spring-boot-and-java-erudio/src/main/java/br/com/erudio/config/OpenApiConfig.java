918b8c6bb30a88e117dc8d05a430f650ab9371cd
leandrocgsi <leandrocgsi@gmail.com>
2022-06-03 22:49:58 -0300

    Continuous Integration with Github, Github Actions, Amazon ECR and ECS

--- 24_ContinuousIntegrationWithAWS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ 24_ContinuousIntegrationWithAWS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("RESTful API with Java 18 and Spring Boot 3")
+				.title("RESTful API with Java 18 and Spring Boot 3 - Foo Bar")
 				.version("v1")
 				.description("Some description about your API")
 				.termsOfService("https://pub.erudio.com.br/meus-cursos")
