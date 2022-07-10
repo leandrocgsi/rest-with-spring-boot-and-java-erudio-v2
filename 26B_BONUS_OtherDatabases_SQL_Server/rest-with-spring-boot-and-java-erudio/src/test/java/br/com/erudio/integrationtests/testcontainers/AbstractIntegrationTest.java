f4386f177d9fdb37ebe68765fa2597759a2de182
leandrocgsi <leandrocgsi@gmail.com>
2022-07-10 17:21:35 -0300

    Connecting to Other Databases - SQL Server

--- 26B_BONUS_OtherDatabases_SQL_Server/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ 26B_BONUS_OtherDatabases_SQL_Server/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.MSSQLServerContainer;
 import org.testcontainers.lifecycle.Startables;
 
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
@@ -16,17 +16,17 @@ public class AbstractIntegrationTest {
 
 	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 		
-		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
+		static MSSQLServerContainer<?> mssql = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:2017-latest").acceptLicense();
 		
 		private static void startContainers() {
-			Startables.deepStart(Stream.of(mysql)).join();
+			Startables.deepStart(Stream.of(mssql)).join();
 		}
 
 		private static Map<String, String> createConnectionConfiguration() {
 			return Map.of(
-				"spring.datasource.url", mysql.getJdbcUrl(),
-				"spring.datasource.username", mysql.getUsername(),
-				"spring.datasource.password", mysql.getPassword()
+				"spring.datasource.url", mssql.getJdbcUrl() + ";encrypt=true;trustServerCertificate=true;",
+				"spring.datasource.username", mssql.getUsername(),
+				"spring.datasource.password", mssql.getPassword()
 			);
 		}
 		
