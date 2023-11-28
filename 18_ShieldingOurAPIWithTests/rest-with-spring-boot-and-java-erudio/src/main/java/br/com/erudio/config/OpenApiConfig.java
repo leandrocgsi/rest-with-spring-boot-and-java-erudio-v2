47fe3526b9e572c8050af60d534b37388927993a
leandrocgsi <leandrocgsi@gmail.com>
2023-11-28 12:35:31 -0300

    Shielding our API with Rest Assured, Testcontainers and JUnit 5

--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -9,21 +9,21 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
-	
-	@Bean
-	OpenAPI customOpenAPI() {
-		return new OpenAPI()
-			.info(new Info()
-				.title("RESTful API with Java 19 and Spring Boot 3.0.1")
-				.version("v1")
-				.description("Some description about your API")
-				.termsOfService("https://pub.erudio.com.br/meus-cursos")
-				.license(
-					new License()
-						.name("Apache 2.0")
-						.url("https://pub.erudio.com.br/meus-cursos")
-					)
-				);
-	}
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST API's RESTful from 0 to AWS  with Java 21,  Spring Boot 3.2.0 and Docker")
+                        .version("v1")
+                        .description("Some description about your API")
+                        .termsOfService("https://pub.erudio.com.br/meus-cursos")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("https://pub.erudio.com.br/meus-cursos")
+                        )
+                );
+    }
 
 }
